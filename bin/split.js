cce4ed0bc52b169416131ab0cd840f39047353dd
Christoph <ignatz@gmail.com>
2018-08-15 22:13:54 -0400

    created movie files for testing

--- bin/split.js
+++ bin/split.js
@@ -1,14 +1,14 @@
-var jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 const filename = '../__tests__/data/movies/single/movies.json'
 
 const determineMovieFilename = ((movie) => {
-  const title = movie.title.replace(/\W/g, '');
+  const title = movie.title.replace(/[^0-9a-zA-Z_ ]/g, '');
   return `${movie.year} - ${title}`;
 })
 
 jsonfile.readFileSync(filename).map(movie => {
   const movieFilename = determineMovieFilename(movie);
-  console.log(movieFilename);
+  console.log('Writing: ', movieFilename);
+  jsonfile.writeFileSync(movieFilename, movie, {spaces: 2})
 });
-
